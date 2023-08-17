96c2587ba354784b30048ef26b38a764fc1fa5a4
koraykoksal <kksl.koray@gmail.com>
2023-08-17 21:36:30 +0300

    KpiCards

--- src/components/KpiCards.jsx
+++ src/components/KpiCards.jsx
@@ -3,55 +3,66 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
 import PaymentsIcon from "@mui/icons-material/Payments"
 import { deepPurple, pink, amber } from "@mui/material/colors"
 import { Paper, Grid, Avatar } from "@mui/material"
+import { Typography } from "@mui/material"
+
+
+
 const KpiCards = () => {
   const cardData = [
     {
       id: 1,
-      icon: <MonetizationOnIcon />,
+      icon: <MonetizationOnIcon sx={{fontSize:'2rem'}} />,
       bgColor: deepPurple[100],
       color: deepPurple[700],
-      title: "sales",
+      title: "SALES",
       value: "$48000",
     },
     {
       id: 2,
-      icon: <ShoppingCartIcon />,
+      icon: <ShoppingCartIcon sx={{fontSize:'2rem'}} />,
       bgColor: pink[100],
       color: pink[700],
-      title: "profit",
+      title: "PROFIT",
       value: "$32000",
     },
     {
       id: 3,
-      icon: <PaymentsIcon />,
+      icon: <PaymentsIcon sx={{fontSize:'2rem'}} />,
       bgColor: amber[100],
       color: amber[700],
-      title: "purchases",
+      title: "PURCHASES",
       value: "$4800",
     },
   ]
 
   return (
-    <div>
-      <Grid container>
+
+      <Grid container justifyContent='center' spacing={2}>
         {cardData.map((item) => (
           <Grid item key={item.id}>
-            <Paper>
+            <Paper sx={{display:'flex',alignItems:'center',gap:2,p:'1rem',width:'350px',justifyContent:'center'}}>
               <Avatar
                 sx={{
                   bgcolor: item.bgColor,
                   color: item.color,
-                  width: 70,
-                  height: 70,
+                  width: '70px',
+                  height: '70px',
                 }}
+                elevation={5}
               >
                 {item.icon}
               </Avatar>
+              <Typography variant="p" mb={2}>
+                {item.title}
+              </Typography>
+              <Typography variant="p" mb={2}>
+                {item.value}
+              </Typography>
             </Paper>
           </Grid>
         ))}
       </Grid>
-    </div>
+
   )
 }
 
