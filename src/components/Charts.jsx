f2fc420eeab028061032f8d80d91eae2fe3f9035
koraykoksal <kksl.koray@gmail.com>
2023-08-17 22:40:40 +0300

    dashboard lineChart

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,34 +1,59 @@
+import { Grid } from "@mui/material";
 import { Card, Title, LineChart } from "@tremor/react";
 import { useSelector } from "react-redux";
-
-const chartdata = [
-
-]
 
 const dataFormatter = (number) =>
   `${Intl.NumberFormat("us").format(number).toString()}%`
 
 const Charts = () => {
 
-  const {sales} = useSelector((state) => state.stock)
+  const {sales,purchases} = useSelector((state) => state.stock)
+  
+  const salesData=sales?.map((item)=>({
+    date: item.createds,
+    quantity: item.quantity,
+    price: Number(item.price_total),
+  }))
 
-  const salesData=sales?.map((item)=>{
+  const purchasesData = purchases?.map((item) => ({
+    date: item.createds,
+    price: Number(item.price_total),
+  }))
 
-  })
+  console.log(salesData)
 
   return (
-    <Card className="mt-4">
-    <Title>Total Sales</Title>
-    <LineChart
-      className="mt-4"
-      data={chartdata}
-      index="year"
-      categories={["Export Growth Rate", "Import Growth Rate"]}
-      colors={["emerald", "gray"]}
-      valueFormatter={dataFormatter}
-      yAxisWidth={40}
-    />
-  </Card>
+    <Grid container justifyContent='center' spacing={3} mt={4}>
+      <Grid item xs={12} md={6}>
+      <Card>
+      <Title>Total Sales</Title>
+      <LineChart
+        className="mt-4"
+        data={salesData}  //x ekseni
+        index="date"
+        categories={["Quantity", "Price"]}
+        colors={["red", "blue"]}
+        valueFormatter={dataFormatter}
+      />
+      </Card>
+      </Grid>
+
+      <Grid item xs={12} md={6}>
+      <Card>
+      <Title>Total Purchases</Title>
+      <LineChart
+        className="mt-4"
+        data={purchasesData}  //x ekseni
+        index="date"
+        categories={["Price"]}
+        colors={["green"]}
+        valueFormatter={dataFormatter}
+      />
+      </Card>
+      </Grid>
+
+    </Grid>
+
   )
 }
 export default Charts
