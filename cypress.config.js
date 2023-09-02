776b0d272840edff379a170daaf0bf045f24d9b3
koraykoksal <kksl.koray@gmail.com>
2023-09-02 13:00:52 +0300

    cypress default timeout and manuel run

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  //*yazılan kodların otomatik test edilmesini kapatır
+  watchForFileChanges:false,
+  //*default kontrol etme süresini ayarlar
+  defaultCommandTimeout:10000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
